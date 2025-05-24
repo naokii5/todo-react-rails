11c2f4b3f0cbb60bbb2ca101b49b8565927ac8e4
naokii5 <ik.11.naoki@gmail.com>
2025-05-24 08:43:28 +0000

    chakuraUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Box, Heading } from '@chakra-ui/react'
 import { TodoForm } from './components/TodoForm'
 import { TodoList } from './components/TodoList'
 
@@ -7,10 +8,12 @@ const queryClient = new QueryClient()
 
 export const App: React.FC = () => (
   <QueryClientProvider client={queryClient}>
-    <div className="max-w-md mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">ToDo App</h1>
+    <Box maxW="md" mx="auto" p={4}>
+      <Heading as="h1" size="xl" mb={4}>
+        ToDo App
+      </Heading>
       <TodoForm />
       <TodoList />
-    </div>
+    </Box>
   </QueryClientProvider>
 )
